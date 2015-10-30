7a559f029e013d623d91b3ede736fa8c2189af00
Nabin Singh <nabinsingh@Nabins-MacBook-Pro.local>
2015-10-30 16:41:52 +1100

    Project initialization

--- PinCode/ViewController.swift
+++ PinCode/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var digit1TextField: UITextField!
+    @IBOutlet weak var digit2TextField: UITextField!
+    @IBOutlet weak var digit3TextField: UITextField!
+    @IBOutlet weak var digit4TextField: UITextField!
+    
+    @IBOutlet weak var codeTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        codeTextField.delegate = self
+        codeTextField.hidden = true
+        codeTextField.becomeFirstResponder()
     }
-
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let code = textField.text! + string
+        
+        switch code.characters.count{
+            case 1:
+                digit1TextField.text = string
+            case 2:
+                digit2TextField.text = string
+            case 3:
+                digit3TextField.text = string
+            case 4:
+                digit4TextField.text = string
+                //submit your code here
+            default: break
+        }
+        return textField.text!.characters.count + (string.characters.count - range.length) <= 4;
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
