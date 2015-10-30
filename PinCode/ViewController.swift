add5e119078a7a1a9b53a339c51ee417c1a15ba0
Nabin Singh <nabinsingh@nabins-macbook-pro.local>
2015-10-30 15:23:41 +1100

    Initial Commit

---
+++ PinCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinCode
+//
+//  Created by Nabin Singh on 30/10/2015.
+//  Copyright © 2015 Nabin Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
